152a2195dabceb4d1b0d33dac24798c1c77ebd62
Bertola lucas <lucasbertola@gmail.com>
2016-04-03 14:56:13 +0200

    getJPGImage

--- exemple/test1.js
+++ exemple/test1.js
@@ -1,6 +1,6 @@
 var hogClass = require("../src/index.js");
 
 var hog = new hogClass();
-hog.getJPGImage("./img/chat1.jpg");
+hog.getJPGImage(__dirname + "/img/chat1.jpg");
 
 //new hog();
--- src/hog.js
+++ src/hog.js
@@ -1,7 +1,29 @@
 function Hog() {
+    this.image = [];
     this.getJPGImage = function(path) {
-        console.log("un path")
+        var jpeg = require('jpeg-js');
+        var fs = require('fs');
+        var jpegData = fs.readFileSync(path);
+        var imageTmp = jpeg.decode(jpegData);
+        var width = imageTmp.width;
+        var heigth = imageTmp.height;
+        this.image = [];
+
+        for (var dataIndex = 0; dataIndex < imageTmp.data.length; dataIndex += 4) {
+            //new line
+            if (dataIndex % (width * 4) == 0) {
+                this.image.push([]);
+            }
+            this.image[this.image.length - 1].push({
+                r: imageTmp.data[dataIndex],
+                g: imageTmp.data[dataIndex + 1],
+                b: imageTmp.data[dataIndex + 2]
+                    //TODO a:imageTmp.data[dataIndex + 3]
+            })
+        }
+
     }
+
 }
 
 module.exports = Hog;
